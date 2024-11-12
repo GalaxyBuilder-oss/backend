a2a8c14e7ded84c804654923d30150813e08f331
GalaxyBuilder-Oss <galaxybuilder.oss@gmail.com>
2024-11-12 17:47:53 +0700

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express"
 import portofolios from "./routes/portofolios.js"
 import auth from "./routes/auth.js"
 import cors from "cors"
+import dotenv from "dotenv"
 import middleware from "./mid.js"
+dotenv.config();
 const app = express()
 const serverPort = process.env.SERVER_PORT
 app.use(express.json())
@@ -14,6 +16,10 @@ app.use(
 
 app.use("/auth", auth)
 app.use("/portofolios", middleware, portofolios)
+
+app.get("/",(req,res)=>{
+    res.send({message:"Hello World", success:true})
+})
 
 app.listen(serverPort, () => {
     console.log(`Server Berjalan Di Port http://localhost:${serverPort}`)
